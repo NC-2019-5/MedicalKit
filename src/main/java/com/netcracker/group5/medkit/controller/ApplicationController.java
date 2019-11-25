b987d135118041e0102cf94f4a105363e0b52e76
AleksandrBut <alex638000@gmail.com>
2019-11-25 16:53:41 +0200

    test commit

--- src/main/java/com/netcracker/group5/medkit/controller/ApplicationController.java
+++ src/main/java/com/netcracker/group5/medkit/controller/ApplicationController.java
@@ -1,6 +1,5 @@
 package com.netcracker.group5.medkit.controller;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class ApplicationController {
 
     @RequestMapping("/")
-    public String hello(){
-        return "Hello, world";
+    public String hello() {
+        return "Hello, world !";
     }
 }
