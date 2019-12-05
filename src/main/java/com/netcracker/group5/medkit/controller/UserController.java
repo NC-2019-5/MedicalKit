1a4e3da496a8e06f2a1c3c37352f96592c174116
AleksandrBut <alex638000@gmail.com>
2019-12-05 23:21:18 +0200

    add JVM option -Duser.timezone=EET

--- src/main/java/com/netcracker/group5/medkit/controller/UserController.java
+++ src/main/java/com/netcracker/group5/medkit/controller/UserController.java
@@ -8,6 +8,8 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.TimeZone;
+
 @RestController
 public class UserController {
 
@@ -17,6 +19,7 @@ public class UserController {
     @RequestMapping("/login")
     @ResponseBody
     public LoginUserResponseItem login(@RequestParam String login, @RequestParam String password) {
+        System.out.println(TimeZone.getDefault());
         return userService.login(login, password);
     }
 }
