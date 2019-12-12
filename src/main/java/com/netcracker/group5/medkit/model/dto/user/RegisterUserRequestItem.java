7855c7b421ac3d9d120e968cde7d21464a2d8d1d
AleksandrBut <alex638000@gmail.com>
2019-12-12 11:04:04 +0200

    add RegisterUser dto classes

---
+++ src/main/java/com/netcracker/group5/medkit/model/dto/user/RegisterUserRequestItem.java
@@ -0,0 +1,103 @@
+package com.netcracker.group5.medkit.model.dto.user;
+
+import com.netcracker.group5.medkit.model.domain.user.Location;
+import com.netcracker.group5.medkit.model.domain.user.Sex;
+
+import java.time.LocalDateTime;
+
+public class RegisterUserRequestItem {
+
+    private String name;
+    private String surname;
+    private LocalDateTime birthDate;
+    private Sex sex;
+    private float weight;
+    private float height;
+    private Location location;
+    private String phoneNumber;
+    private String email;
+    private String password;
+
+    public RegisterUserRequestItem() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public LocalDateTime getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(LocalDateTime birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    public Sex getSex() {
+        return sex;
+    }
+
+    public void setSex(Sex sex) {
+        this.sex = sex;
+    }
+
+    public float getWeight() {
+        return weight;
+    }
+
+    public void setWeight(float weight) {
+        this.weight = weight;
+    }
+
+    public float getHeight() {
+        return height;
+    }
+
+    public void setHeight(float height) {
+        this.height = height;
+    }
+
+    public Location getLocation() {
+        return location;
+    }
+
+    public void setLocation(Location location) {
+        this.location = location;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
---
+++ src/main/java/com/netcracker/group5/medkit/model/dto/user/RegisterUserResponseItem.java
@@ -0,0 +1,4 @@
+package com.netcracker.group5.medkit.model.dto.user;
+
+public class RegisterUserResponseItem {
+}
