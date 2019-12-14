68b4226ed21440b1f882c857b3c7904daa162efe
AleksandrBut <alex638000@gmail.com>
2019-12-14 20:26:10 +0200

    delete unnecessary dto classes from user package

--- src/main/java/com/netcracker/group5/medkit/model/dto/user/AdministratorDto.java
+++
@@ -1,4 +0,0 @@
-package com.netcracker.group5.medkit.model.dto.user;
-
-public class AdministratorDto {
-}
--- src/main/java/com/netcracker/group5/medkit/model/dto/user/PatientDto.java
+++
@@ -1,7 +0,0 @@
-package com.netcracker.group5.medkit.model.dto.user;
-
-import com.netcracker.group5.medkit.model.domain.Requestable;
-
-public class PatientDto {
-
-}
