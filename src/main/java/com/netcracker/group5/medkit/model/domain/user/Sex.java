8a10355aba5cc3126733cc2c729f25b15b62086f
AleksandrBut <alex638000@gmail.com>
2019-12-21 12:15:07 +0200

    remove String values from enun

--- src/main/java/com/netcracker/group5/medkit/model/domain/user/Sex.java
+++ src/main/java/com/netcracker/group5/medkit/model/domain/user/Sex.java
@@ -1,16 +1,6 @@
 package com.netcracker.group5.medkit.model.domain.user;
 
 public enum Sex {
-    MALE("Male"),
-    FEMALE("Female");
-
-    private String name;
-
-    Sex(String name) {
-        this.name = name;
-    }
-
-    public String getName() {
-        return name;
-    }
+    MALE,
+    FEMALE;
 }
