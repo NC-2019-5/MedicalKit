43723efa8e9b5fc7320df5575583b64759a2f741
AleksandrBut <alex638000@gmail.com>
2019-12-21 12:01:28 +0200

    add String values to enum

--- src/main/java/com/netcracker/group5/medkit/model/domain/user/Sex.java
+++ src/main/java/com/netcracker/group5/medkit/model/domain/user/Sex.java
@@ -1,6 +1,16 @@
 package com.netcracker.group5.medkit.model.domain.user;
 
 public enum Sex {
-    MALE,
-    FEMALE
+    MALE("Male"),
+    FEMALE("Female");
+
+    private String name;
+
+    Sex(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
 }
