2e7f71aaca571ae6b29800b6a4918e0d3a7cc069
AleksandrBut <alex638000@gmail.com>
2019-12-24 23:54:46 +0200

    make UserService extend UserDetailsService

--- src/main/java/com/netcracker/group5/medkit/service/UserServiceImpl.java
+++ src/main/java/com/netcracker/group5/medkit/service/UserServiceImpl.java
@@ -2,8 +2,13 @@ package com.netcracker.group5.medkit.service;
 
 import com.netcracker.group5.medkit.model.domain.user.User;
 import com.netcracker.group5.medkit.repository.UserRepository;
+import com.netcracker.group5.medkit.security.CustomUserDetails;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class UserServiceImpl implements UserService {
@@ -19,11 +24,16 @@ public class UserServiceImpl implements UserService {
             return "Login successful! Email: " + user.getEmail() + " , password: " + user.getPassword();
         }
 
-        return "Can not login. Wrong email or password";
+        return ":(";
     }
 
     @Override
     public User registerUser(User user) {
         return userRepository.save(user);
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        return new CustomUserDetails(userRepository.findUserByEmail(email));
     }
 }
