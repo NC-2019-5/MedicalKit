a846bc9975c4302d4676fd47db99ae6315ae68a5
AleksandrBut <alex638000@gmail.com>
2019-12-24 23:56:33 +0200

    add TokenAuthService class

---
+++ src/main/java/com/netcracker/group5/medkit/security/TokenAuthService.java
@@ -0,0 +1,28 @@
+package com.netcracker.group5.medkit.security;
+
+import com.netcracker.group5.medkit.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Service;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.Optional;
+
+@Service
+public class TokenAuthService {
+    public static final String AUTH_HEADER_NAME = "X-Auth-Token";
+
+    @Autowired
+    private TokenHandler tokenHandler;
+
+    @Autowired
+    private UserService userService;
+
+    public Optional<Authentication> getAuthentication(HttpServletRequest request) {
+        return Optional
+                .ofNullable(request.getHeader(AUTH_HEADER_NAME))
+                .flatMap(tokenHandler::extractUserEmail)
+                .flatMap(userService::loadUserByUsername)
+                .map(UserAuthentication::new);
+    }
+}
