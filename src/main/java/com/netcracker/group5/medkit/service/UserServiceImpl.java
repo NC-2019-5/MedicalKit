292d2f3071c9c441841e74425200e03a508fab7a
AleksandrBut <alex638000@gmail.com>
2019-12-25 20:49:51 +0200

    delete security configuration

--- src/main/java/com/netcracker/group5/medkit/service/UserServiceImpl.java
+++ src/main/java/com/netcracker/group5/medkit/service/UserServiceImpl.java
@@ -2,7 +2,6 @@ package com.netcracker.group5.medkit.service;
 
 import com.netcracker.group5.medkit.model.domain.user.User;
 import com.netcracker.group5.medkit.repository.UserRepository;
-import com.netcracker.group5.medkit.security.CustomUserDetails;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -28,14 +27,5 @@ public class UserServiceImpl implements UserService {
     @Override
     public User registerUser(User user) {
         return userRepository.save(user);
-    }
-
-    @Override
-    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
-        try {
-            return new CustomUserDetails(userRepository.findUserByEmail(email));
-        } catch (Exception e) {
-            throw new UsernameNotFoundException("User with email " + email + " wan not found.");
-        }
     }
 }
