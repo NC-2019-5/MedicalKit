e25bc116d7cd2f7572a13e34dcad991790ee46bb
AleksandrBut <alex638000@gmail.com>
2020-01-02 22:54:01 +0200

    add PurchaseRepository and implementation with test call

---
+++ src/main/java/com/netcracker/group5/medkit/repository/PurchaseRepository.java
@@ -0,0 +1,10 @@
+package com.netcracker.group5.medkit.repository;
+
+import com.netcracker.group5.medkit.model.domain.purchase.PurchaseItem;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface PurchaseRepository {
+    Optional<List<PurchaseItem>> findPurchaseItems(int limit, long offset, String searchQuery);
+}
