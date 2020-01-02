8573c2266152ff764885e99c90d65e710a04e124
AleksandrBut <alex638000@gmail.com>
2020-01-02 22:53:05 +0200

    add PurchaseService and implementation

---
+++ src/main/java/com/netcracker/group5/medkit/service/PurchaseService.java
@@ -0,0 +1,11 @@
+package com.netcracker.group5.medkit.service;
+
+import com.netcracker.group5.medkit.model.domain.purchase.PurchaseItem;
+import org.springframework.data.domain.Pageable;
+
+
+import java.util.List;
+
+public interface PurchaseService {
+    List<PurchaseItem> findPurchaseItems(Pageable pageable, String searchQuery);
+}
