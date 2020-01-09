06fab3067d30559d44a7b73dc2a6da586e14c30d
AleksandrBut <alex638000@gmail.com>
2020-01-09 21:17:06 +0200

    add UserController.editPassword method

--- src/main/java/com/netcracker/group5/medkit/controller/UserController.java
+++ src/main/java/com/netcracker/group5/medkit/controller/UserController.java
@@ -1,27 +1,18 @@
 package com.netcracker.group5.medkit.controller;
 
 import com.netcracker.group5.medkit.model.domain.user.Patient;
-import com.netcracker.group5.medkit.model.dto.user.EditPasswordRequestItem;
 import com.netcracker.group5.medkit.model.domain.user.User;
-import com.netcracker.group5.medkit.model.dto.user.AuthTokenResponse;
-import com.netcracker.group5.medkit.model.dto.user.LoginUserRequestItem;
-import com.netcracker.group5.medkit.model.dto.user.RegisterPatientRequestItem;
-import com.netcracker.group5.medkit.model.dto.user.RegisterPatientResponseItem;
+import com.netcracker.group5.medkit.model.dto.user.*;
 import com.netcracker.group5.medkit.security.TokenHandler;
 import com.netcracker.group5.medkit.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
 
 @CrossOrigin
@@ -58,5 +49,14 @@ public class UserController {
     public ResponseEntity<?> registerUser(@Valid @RequestBody RegisterPatientRequestItem registerPatientRequestItem) {
         Patient patient = (Patient) userService.registerUser(new Patient(registerPatientRequestItem));
         return ResponseEntity.ok(new RegisterPatientResponseItem(patient.getEmail()));
+    }
+
+    @PutMapping("/profile/change-password")
+    public ResponseEntity<?> editPassword(@Valid @RequestBody EditPasswordRequestItem requestItem, HttpServletRequest httpServletRequest) {
+        String userEmail = tokenHandler.extractUserEmail(tokenHandler.getTokenFromHttpRequest(httpServletRequest));
+        User user = userService.getUserByEmail(userEmail);
+
+        userService.editPassword(user, requestItem.getOldPassword(), requestItem.getNewPassword());
+        return ResponseEntity.ok().build();
     }
 }
