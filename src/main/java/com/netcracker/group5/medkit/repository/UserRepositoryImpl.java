25f0068ed9ced8e4289e231871a4e9dfb5fe1156
AleksandrBut <alex638000@gmail.com>
2020-01-10 15:23:31 +0200

    add UserRepository.saveUser method

--- src/main/java/com/netcracker/group5/medkit/repository/UserRepositoryImpl.java
+++ src/main/java/com/netcracker/group5/medkit/repository/UserRepositoryImpl.java
@@ -75,8 +75,48 @@ public class UserRepositoryImpl extends JdbcDaoSupport implements UserRepository
      * (depends on user.role)
      */
     @Override
-    public User save(User user) {
-        return patientRepository.save((Patient) user);
+    public User saveByRole(User user) {
+        if (user.getRole().equals(Role.PATIENT)) {
+            User savedUser = saveUser(user);
+
+            Patient patientToBeSaved = patientRepository.findByUserId(savedUser.getId());
+            Patient editedPatient = (Patient) user;
+
+            editedPatient.setPassword(savedUser.getPassword());
+            editedPatient.setRole(savedUser.getRole());
+            editedPatient.setId(patientToBeSaved.getId());
+            editedPatient.setSex(patientToBeSaved.getSex());
+
+            Patient savedPatient = patientRepository.save(savedUser.getId(), (Patient) user);
+
+            savedPatient.setEmail(savedUser.getEmail());
+            savedPatient.setPassword(savedUser.getPassword());
+            savedPatient.setRole(savedUser.getRole());
+
+            return savedPatient;
+        }
+
+        throw new IllegalArgumentException("User role" + user.getRole() + " does not exist");
+    }
+
+    public User saveUser(User user) {
+        SqlParameterSource parameterSource = new MapSqlParameterSource()
+                .addValue("p_user_object_id", user.getId())
+                .addValue("p_user_email", user.getEmail())
+                .addValue("p_user_password", user.getPassword())
+                .addValue("p_user_role", user.getRole().getRoleName());
+
+        Map<String, Object> result = new SimpleJdbcCall(jdbcTemplate)
+                .withCatalogName("USER_PKG")
+                .withProcedureName("saveUserObject")
+                .execute(parameterSource);
+
+        return User.newUserBuilder()
+                .setId(((BigDecimal) result.get("p_user_object_id")).longValue())
+                .setEmail(result.get("p_user_email").toString())
+                .setPassword(result.get("p_user_password").toString())
+                .setRole(Role.getRoleByName((String) result.get("p_user_role")))
+                .build();
     }
 
     @Override
