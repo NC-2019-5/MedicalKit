dac64f885244112c078a2470ebbd66d6f80cfce3
AleksandrBut <alex638000@gmail.com>
2020-01-11 13:01:23 +0200

    fix cors preflights error

--- src/main/java/com/netcracker/group5/medkit/security/SecurityConfigurer.java
+++ src/main/java/com/netcracker/group5/medkit/security/SecurityConfigurer.java
@@ -29,7 +29,10 @@ public class SecurityConfigurer extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.csrf().disable()
+        http
+                .csrf().disable()
+                .cors()
+                .and()
                 .authorizeRequests().antMatchers("/login", "/register").permitAll()
                 .anyRequest().authenticated()
                 .and()
