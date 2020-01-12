ccc0429787d6ec6d159edee066b9a50e67d70aca
AleksandrBut <alex638000@gmail.com>
2020-01-12 16:14:31 +0200

    pass patient id to PurchaseServiceImpl.findPurchaseItems

--- src/main/java/com/netcracker/group5/medkit/service/PurchaseServiceImpl.java
+++ src/main/java/com/netcracker/group5/medkit/service/PurchaseServiceImpl.java
@@ -1,7 +1,9 @@
 package com.netcracker.group5.medkit.service;
 
 import com.netcracker.group5.medkit.model.domain.purchase.PurchaseItem;
+import com.netcracker.group5.medkit.model.domain.user.Patient;
 import com.netcracker.group5.medkit.model.domain.user.User;
+import com.netcracker.group5.medkit.repository.PatientRepository;
 import com.netcracker.group5.medkit.repository.PurchaseRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Pageable;
@@ -16,10 +18,16 @@ public class PurchaseServiceImpl implements PurchaseService {
     @Autowired
     private PurchaseRepository purchaseRepository;
 
+    @Autowired
+    private PatientRepository patientRepository;
+
     @Override
     public List<PurchaseItem> findPurchaseItems(Pageable pageable, String searchQuery) {
         User currentUser = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
-        return purchaseRepository.findPurchaseItems(currentUser.getId(), pageable.getPageSize(), pageable.getOffset(), searchQuery).orElse(null);
+        Patient patient = patientRepository.findByUserId(currentUser.getId());
+
+        return purchaseRepository.findPurchaseItems(patient.getId(), pageable.getPageSize(), pageable.getOffset(), searchQuery)
+                .orElse(null);
     }
 
     @Override
