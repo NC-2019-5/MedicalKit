21330433fffb4975492456d56829cfe30768b21f
AleksandrBut <alex638000@gmail.com>
2020-01-12 22:35:09 +0200

    make "search query" request parameter not required

--- src/main/java/com/netcracker/group5/medkit/controller/PurchaseController.java
+++ src/main/java/com/netcracker/group5/medkit/controller/PurchaseController.java
@@ -1,5 +1,6 @@
 package com.netcracker.group5.medkit.controller;
 
+import com.netcracker.group5.medkit.model.domain.purchase.PurchaseItem;
 import com.netcracker.group5.medkit.model.dto.purchase.AddPurchaseItemRequest;
 import com.netcracker.group5.medkit.model.dto.purchase.FindPurchaseItemsResponse;
 import com.netcracker.group5.medkit.service.PurchaseService;
@@ -10,6 +11,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @CrossOrigin
 @Controller
 public class PurchaseController {
@@ -18,20 +21,25 @@ public class PurchaseController {
     private PurchaseService purchaseService;
 
     @GetMapping("/profile/purchases")
-    public ResponseEntity<?> findPurchaseItems(@PageableDefault Pageable pageable, @RequestParam String searchQuery) {
-        FindPurchaseItemsResponse responseItem = new FindPurchaseItemsResponse(purchaseService.findPurchaseItems(pageable, searchQuery));
+    public ResponseEntity<?> findPurchaseItems(@PageableDefault Pageable pageable, @RequestParam(required = false) String searchQuery) {
+        List<PurchaseItem> purchaseItems = purchaseService.findPurchaseItems(pageable, searchQuery);
+        FindPurchaseItemsResponse responseItem = new FindPurchaseItemsResponse(purchaseItems);
+
         return ResponseEntity.ok(responseItem);
     }
 
     @PostMapping("/profile/purchases")
     public ResponseEntity<?> addPurchaseItem(@RequestBody AddPurchaseItemRequest addPurchaseItemRequest) {
-        purchaseService.addPurchaseItem(addPurchaseItemRequest.getPurchaseItem());
+        PurchaseItem purchaseItem = new PurchaseItem(addPurchaseItemRequest);
+        purchaseService.addPurchaseItem(purchaseItem);
+
         return ResponseEntity.ok().build();
     }
 
     @DeleteMapping("/profile/purchases")
     public ResponseEntity<?> deletePurchaseItem(@RequestParam Long id) {
         purchaseService.deletePurchaseItem(id);
+
         return ResponseEntity.ok().build();
     }
 }
