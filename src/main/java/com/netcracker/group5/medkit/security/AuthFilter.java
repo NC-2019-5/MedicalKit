1d6289b42bd4d89909402d504b4fcbdbe220b298
AleksandrBut <alex638000@gmail.com>
2020-01-15 23:25:34 +0200

    fix password encoding

--- src/main/java/com/netcracker/group5/medkit/security/AuthFilter.java
+++ src/main/java/com/netcracker/group5/medkit/security/AuthFilter.java
@@ -48,7 +48,7 @@ public class AuthFilter extends OncePerRequestFilter {
 
             if (tokenHandler.validateToken(token, user)) {
                 UsernamePasswordAuthenticationToken authenticationToken = new
-                        UsernamePasswordAuthenticationToken(user, user.getPassword());
+                        UsernamePasswordAuthenticationToken(user, user.getPassword(), user.getAuthorities());
                 authenticationToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(httpServletRequest));
                 SecurityContextHolder.getContext().setAuthentication(authenticationToken);
             } else {
