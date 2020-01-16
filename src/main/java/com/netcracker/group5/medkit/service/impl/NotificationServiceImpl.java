7245252a38d56d277f97b559e7ec7cb8e88fa861
AleksandrBut <alex638000@gmail.com>
2020-01-16 22:39:42 +0200

    create NotificationService and implementation

---
+++ src/main/java/com/netcracker/group5/medkit/service/impl/NotificationServiceImpl.java
@@ -0,0 +1,20 @@
+package com.netcracker.group5.medkit.service.impl;
+
+import com.netcracker.group5.medkit.repository.NotificationRepository;
+import com.netcracker.group5.medkit.service.NotificationService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class NotificationServiceImpl implements NotificationService {
+
+    @Autowired
+    private NotificationRepository notificationRepository;
+
+    @Override
+    public void bulkCreateNotifications(Long userId, List<Long> prescriptionItemIdList) {
+        notificationRepository.bulkCreateNotifications(userId, prescriptionItemIdList);
+    }
+}
