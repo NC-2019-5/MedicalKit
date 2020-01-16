e5b505fe6ea184c52e1854358f14b77d9aa5b7fc
AleksandrBut <alex638000@gmail.com>
2020-01-16 22:40:17 +0200

    create NotificationRepository and implementation

---
+++ src/main/java/com/netcracker/group5/medkit/repository/NotificationRepository.java
@@ -0,0 +1,7 @@
+package com.netcracker.group5.medkit.repository;
+
+import java.util.List;
+
+public interface NotificationRepository {
+    void bulkCreateNotifications(Long userId, List<Long> prescriptionItemIdList);
+}
