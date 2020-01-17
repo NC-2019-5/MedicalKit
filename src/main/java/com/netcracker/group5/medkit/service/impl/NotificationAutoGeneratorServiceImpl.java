84227ee44aba173737f76f709f1d1d2e3a38f592
AleksandrBut <alex638000@gmail.com>
2020-01-17 17:11:02 +0200

    change NotificationAutoGeneratorService.generateNotification method signature

---
+++ src/main/java/com/netcracker/group5/medkit/service/impl/NotificationAutoGeneratorServiceImpl.java
@@ -0,0 +1,29 @@
+package com.netcracker.group5.medkit.service.impl;
+
+import com.netcracker.group5.medkit.model.domain.user.Patient;
+import com.netcracker.group5.medkit.service.NotificationAutoGeneratorService;
+import com.netcracker.group5.medkit.service.NotificationService;
+import com.netcracker.group5.medkit.service.PatientService;
+import com.netcracker.group5.medkit.service.PrescriptionService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class NotificationAutoGeneratorServiceImpl implements NotificationAutoGeneratorService {
+
+    @Autowired
+    private PatientService patientService;
+
+    @Autowired
+    private NotificationService notificationService;
+
+    @Autowired
+    private PrescriptionService prescriptionService;
+
+    @Override
+    public void generateNotification(Long userId) {
+        Patient patient = patientService.getPatientByUserId(userId);
+
+        notificationService.bulkCreateNotifications(userId, prescriptionService.findActivePrescriptionItems(patient.getId()));
+    }
+}
