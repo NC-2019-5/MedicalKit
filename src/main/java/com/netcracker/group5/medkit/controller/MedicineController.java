e8ac9ab48dd2d0d18d9c2628743b6e8d854df263
AleksandrBut <alex638000@gmail.com>
2020-01-19 19:52:35 +0200

    configure dto validation for Medicine

--- src/main/java/com/netcracker/group5/medkit/controller/MedicineController.java
+++ src/main/java/com/netcracker/group5/medkit/controller/MedicineController.java
@@ -1,21 +1,23 @@
 package com.netcracker.group5.medkit.controller;
 
 import com.netcracker.group5.medkit.model.domain.medicine.Medicine;
-import com.netcracker.group5.medkit.model.domain.medicine.MedicineInstance;
 import com.netcracker.group5.medkit.model.dto.medicine.EditMedicineRequestItem;
 import com.netcracker.group5.medkit.model.dto.medicine.FindMedicinesResponseItem;
-import com.netcracker.group5.medkit.model.dto.medicine.MedicineInstanceRequestItem;
-import com.netcracker.group5.medkit.model.dto.medicine.MedicineRequestItem;
+import com.netcracker.group5.medkit.model.dto.medicine.SaveMedicineRequestItem;
 import com.netcracker.group5.medkit.service.MedicineService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Positive;
 import java.util.List;
 
+@Validated
 @CrossOrigin
 @RestController
 public class MedicineController {
@@ -23,33 +25,44 @@ public class MedicineController {
     @Autowired
     private MedicineService medicineService;
 
-    @GetMapping("/all-medicines/all")
+    @GetMapping("/all-medicines")
     public ResponseEntity<?> findAllMedicines(@PageableDefault Pageable pageable) {
         List<Medicine> medicines = medicineService.findAllMedicines(pageable);
+
         return ResponseEntity.ok(new FindMedicinesResponseItem(medicines));
     }
 
-    @GetMapping("/all-medicines/one")
-    public ResponseEntity<?> findMedicine(@RequestParam Long id) {
+    @GetMapping("/all-medicines/{id}")
+    public ResponseEntity<?> findMedicine(
+            @NotNull(message = "Id cannot be empty")
+            @Positive(message = "Id must be greater than 0")
+            @PathVariable Long id) {
         Medicine medicine = medicineService.findMedicine(id);
+
         return ResponseEntity.ok(medicine);
     }
 
-    @DeleteMapping("/all-medicines/delete")
-    public ResponseEntity<?> deleteMedicine(@RequestParam Long id) {
+    @DeleteMapping("/all-medicines")
+    public ResponseEntity<?> deleteMedicine(
+            @NotNull(message = "Id cannot be empty")
+            @Positive(message = "Id must be greater than 0")
+            @RequestParam Long id) {
         medicineService.deleteMedicine(id);
+
         return ResponseEntity.ok().build();
     }
 
-    @PostMapping("/all-medicines/add")
-    public ResponseEntity<?> createMedicine(@RequestBody MedicineRequestItem medicineRequestItem) {
-        Medicine medicine = medicineService.saveMedicine(new Medicine(medicineRequestItem));
+    @PostMapping("/all-medicines")
+    public ResponseEntity<?> saveMedicine(@Valid @RequestBody SaveMedicineRequestItem requestItem) {
+        Medicine medicine = medicineService.saveMedicine(new Medicine(requestItem));
+
         return ResponseEntity.ok(medicine);
     }
 
-    @PutMapping("/all-medicines/edit")
-    public ResponseEntity<?> editMedicine(@RequestBody EditMedicineRequestItem editMedicineRequestItem) {
-        Medicine medicine = medicineService.saveMedicine(new Medicine(editMedicineRequestItem));
+    @PutMapping("/all-medicines")
+    public ResponseEntity<?> editMedicine(@Valid @RequestBody EditMedicineRequestItem requestItem) {
+        Medicine medicine = medicineService.saveMedicine(new Medicine(requestItem));
+
         return ResponseEntity.ok(medicine);
     }
 }
