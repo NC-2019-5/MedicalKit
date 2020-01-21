97a1e1bcfc1e89cd188fce435fdaa05be3f3584c
Yamiguri <ududenko.s98@gmail.com>
2020-01-21 23:52:44 +0200

    fix SecurityConfigurer, NotificationService, port = 8080

--- src/main/java/com/netcracker/group5/medkit/service/impl/NotificationServiceImpl.java
+++ src/main/java/com/netcracker/group5/medkit/service/impl/NotificationServiceImpl.java
@@ -37,5 +37,6 @@ public class NotificationServiceImpl implements NotificationService {
             medicineInstanceService.saveMedicineInstance(medicineInstance);
             notificationRepository.deleteNotification(confirmedNotification.getId());
         }
+        throw new IllegalArgumentException();
     }
 }
