97f244f8db1b53c3d8375bbb00cfafa01049e91a
Yamiguri <ududenko.s98@gmail.com>
2020-01-21 16:13:41 +0200

    add auto-decrement

--- src/main/java/com/netcracker/group5/medkit/service/impl/NotificationServiceImpl.java
+++ src/main/java/com/netcracker/group5/medkit/service/impl/NotificationServiceImpl.java
@@ -1,7 +1,9 @@
 package com.netcracker.group5.medkit.service.impl;
 
+import com.netcracker.group5.medkit.model.domain.medicine.MedicineInstance;
 import com.netcracker.group5.medkit.model.domain.request.Notification;
 import com.netcracker.group5.medkit.repository.NotificationRepository;
+import com.netcracker.group5.medkit.service.MedicineInstanceService;
 import com.netcracker.group5.medkit.service.NotificationService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -13,6 +15,9 @@ public class NotificationServiceImpl implements NotificationService {
 
     @Autowired
     private NotificationRepository notificationRepository;
+
+    @Autowired
+    private MedicineInstanceService medicineInstanceService ;
 
     @Override
     public void bulkCreateNotifications(Long userId, List<Long> prescriptionItemIdList) {
@@ -26,6 +31,11 @@ public class NotificationServiceImpl implements NotificationService {
 
     @Override
     public void autoDecrementMI(Notification confirmedNotification) {
-
+        MedicineInstance medicineInstance = confirmedNotification.getMedicineInstance();
+        if(medicineInstance.getAmount() >= confirmedNotification.getPrescriptionItem().getDosage()){
+            medicineInstance.setAmount(medicineInstance.getAmount() - confirmedNotification.getPrescriptionItem().getDosage());
+            medicineInstanceService.saveMedicineInstance(medicineInstance);
+            notificationRepository.deleteNotification(confirmedNotification.getId());
+        }
     }
 }
