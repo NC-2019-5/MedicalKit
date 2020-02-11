8c40338734b22c702f9498022a6a8958991451b3
AleksandrBut <alex638000@gmail.com>
2020-02-11 12:09:59 +0200

    wdjgsadjkasvfshavfuyqvfqwhjbshavfjgqvwuiegqgwevwq

--- src/main/java/com/netcracker/group5/medkit/model/domain/user/Patient.java
+++ src/main/java/com/netcracker/group5/medkit/model/domain/user/Patient.java
@@ -40,7 +40,6 @@ public class Patient extends User {
     }
 
     public Patient(EditPatientRequestItem requestItem) {
-        this.email = requestItem.getEmail();
         this.name = requestItem.getFirstName();
         this.surname = requestItem.getLastName();
         this.birthDate = requestItem.getDateOfBirth();
